58c333985c34fd13421a85a1e0c14daa8c692b50
SaiGanesh-Coder <Saiganesh.Kudipudi@gmail.com>
2024-08-15 03:20:12 +0530

    Added ResetPassword Controller and Auth Middleware

--- server/models/User.js
+++ server/models/User.js
@@ -34,6 +34,12 @@ const userSchema = new mongoose.Schema({
         type: mongoose.Schema.Types.ObjectId,
         ref: "Course"
     },
+    token: {
+        type: String,
+    },
+    resetPasswordExpires: {
+        type: Date,
+    },
     image: {
         type: String,
         required: true
